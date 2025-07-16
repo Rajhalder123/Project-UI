1853dcac3fbc021b2ee56d2cdbbf2ad683bd3d56
Raj Halder <rajhalder8000@gmail.com>
2025-07-16 12:38:04 +0530

    second chg

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+import React from 'react'
+
+const main = () => {
+  return (
+    <div>main</div>
+  )
+}
+
+=======
+import React from 'react'
+
+const main = () => {
+  return (
+    <div>main</div>
+  )
+}
+
+>>>>>>> 2bbdbc8fcd857699331372607d7bdacc2dde7269
+export default main
